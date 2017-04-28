b7385011d8da1448ad5aa41a48ce8abd1cecb66f
Florian Schliep <florian@appiculous.com>
2017-04-28 16:16:54 +0200

    Initial Commit

---
+++ FLORouter/FLORouter.h
@@ -0,0 +1,19 @@
+//
+//  FLORouter.h
+//  FLORouter
+//
+//  Created by Florian Schliep on 28.04.17.
+//  Copyright © 2017 Florian Schliep. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FLORouter.
+FOUNDATION_EXPORT double FLORouterVersionNumber;
+
+//! Project version string for FLORouter.
+FOUNDATION_EXPORT const unsigned char FLORouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FLORouter/PublicHeader.h>
+
+
